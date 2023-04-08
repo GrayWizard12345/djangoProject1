965b5ea66845fc8e31687965817370178ca7cdf7
GrayWizard12345 <muslim4ik14@gmail.com>
2023-04-08 11:40:54 +0500

    Second template added

--- poll/models.py
+++ poll/models.py
@@ -5,6 +5,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=256)
     creation_date = models.DateField(auto_now=True)
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(
@@ -13,3 +16,6 @@ class Choice(models.Model):
     )
     choice_text = models.CharField(max_length=256)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
